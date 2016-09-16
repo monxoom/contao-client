713861239790cf48391b45d35a91d24827ae395c
Hendrik Obermayer <h.obermayer@comolo.de>
2016-09-16 22:03:41 +0200

    removed some old files, added readme, fixed bug with missing abs. url

---
+++ README.md
@@ -0,0 +1,5 @@
+Add the following lines to: app/config/routing.yml
+```yaml
+ComoloSuperLoginClientBundle:
+    resource: "@ComoloSuperLoginClientBundle/Resources/config/routing.yml"
+```
--- src/Model/SuperLoginServerModel.php
+++ src/Model/SuperLoginServerModel.php
@@ -11,35 +11,19 @@
  * @copyright 2014 Hendrik Obermayer
  */
 
-
-/**
- * Namespace
- */
 namespace Comolo\SuperLoginClient\ContaoEdition\Model;
+
 use  Comolo\SuperLoginClient\ContaoEdition\AuthProvider\OAuth2Provider;
 
-/**
- * Class AuthServerModel
- *
- * @copyright  2014 Hendrik Obermayer
- * @author     Hendrik Obermayer - Comolo GmbH
- * @package    Devtools
- */
 class SuperLoginServerModel extends \Model
 {
-
-	/**
-	 * Name of the table
-	 * @var string
-	 */
 	protected static $strTable = 'tl_superlogin_server';
-    
+
     public function getRedirectUrl()
     {
         return \System::getContainer()->get('router')
                 ->generate('superlogin_auth_redirect', array('serverId' => $this->id));
     }
-
 }
 
 /*
