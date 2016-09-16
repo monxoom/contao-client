63269e467260350e7088a907c440f51906f2662e
Hendrik Obermayer <h.obermayer@comolo.de>
2016-09-16 22:22:10 +0200

    fixed container-request call

--- src/Controller/AuthorizationController.php
+++ src/Controller/AuthorizationController.php
@@ -17,68 +17,68 @@ class AuthorizationController extends Controller
 {
     /**
      * Redirect the user to the oauth server
-     * 
+     *
      */
     public function redirectAction($serverId)
     {
-        $server = $this->get('superlogin.server_manager')->find($serverId); 
-        
+        $server = $this->get('superlogin.server_manager')->find($serverId);
+
         // Server not found
         if (!$server) {
             throw new AccessDeniedHttpException('Unknown server');
         }
-        
+
         // Create oauth client instance
         $provider = $this->get('superlogin.server_manager')->createOAuth2Provider($server);
         $authorizationUrl = $provider->getAuthorizationUrl();
-        
+
         // Store state in session
         $this->get('session')->set('oauth2state', $provider->getState());
-        
+
         // Redirect user
         return new RedirectResponse($authorizationUrl, 302);
-        
+
     }
-    
+
     public function authorizationAction($serverId)
     {
-        $server = $this->get('superlogin.server_manager')->find($serverId);  
-        $request = $this->get('request');
+        $server = $this->get('superlogin.server_manager')->find($serverId);
+        $request = $this->get('request_stack')->getCurrentRequest();
         $state = $request->query->get('state');
         $state_session = $this->get('session')->get('oauth2state');
-        
+
         // Server not found
         if (!$server) {
             throw new AccessDeniedHttpException('Unknown server');
         }
-        
+
         // Init provider
         $provider = $this->get('superlogin.server_manager')->createOAuth2Provider($server);
-        
+
         // Validate state
         if (empty($state) || ($state !== $state_session)) {
             $this->get('session')->remove('oauth2state');
             throw new AccessDeniedHttpException('Invalid state');
         }
-        
+
         try {
-            
+
             // Get Access token
             $accessToken = $provider->getAccessToken('authorization_code', [
                 'code' => $request->query->get('code')
             ]);
-            
+
             // Get Resource Owner
             $resourceOwner = $provider->getResourceOwner($accessToken);
             $userDetails = $resourceOwner->toArray()['user'];
-            
+
             // Simulate Contao login
             $contaoUser = $this->get('superlogin.remote_user')->create($userDetails);
             $this->get('superlogin.remote_user')->createOrUpdate($contaoUser);
             $this->get('superlogin.remote_user')->loginAs($contaoUser);
-            
+
             return $this->redirectToRoute('contao_backend');
-             
+
         } catch (IdentityProviderException $e) {
             // Failed to get the access token or user details.
             throw new AccessDeniedHttpException('an error occured');
--- src/Foundation/Security/ContaoBackendLogin.php
+++ src/Foundation/Security/ContaoBackendLogin.php
@@ -17,49 +17,49 @@ class ContaoBackendLogin
     protected $container;
     static protected $grantLogin = false;
     static protected $grantLoginUsername = '';
-    
+
     public function setContainer(Container $container)
     {
         $this->container = $container;
     }
-    
+
     public function setRemoteUser(RemoteUserInterface $remoteUser)
     {
         $this->remoteUser = $remoteUser;
     }
-      
+
     public function login()
     {
         $this->container->get('contao.framework')->initialize();
         $user = ContaoBackendUser::getInstance();
-        
+
         // Prepare for login
         $_POST['username'] = $this->remoteUser->getUsername();
         $_POST['password'] = '####';
-        
-        // Activate password hook 
+
+        // Activate password hook
         self::$grantLogin = true;
         self::$grantLoginUsername = $this->remoteUser->getUsername();
-        
+
         // Add contao hook
         $GLOBALS['TL_HOOKS']['checkCredentials'][] = [
-            'Comolo\SuperLoginClient\ContaoEdition\Foundation\Security\ContaoBackendLogin', 
+            'Comolo\SuperLoginClient\ContaoEdition\Foundation\Security\ContaoBackendLogin',
             'hookCheckCredentials'
         ];
-        
+
         return $user->login();
-        
-        /*    
+
+        /*
         // Create token
         $token = new ContaoToken($contaoUser);
         $this->get('security.context')->setToken($token);
-        
-        $request = $this->get('request');
+
+        $request = $this->get('request_stack')->getCurrentRequest();
         $event = new InteractiveLoginEvent($request, $token);
         $this->get('event_dispatcher')->dispatch('security.interactive_login', $event);
         */
     }
-    
+
     public function hookCheckCredentials($username, $password)
     {
         return (self::$grantLogin && self::$grantLoginUsername == $username);
