0892711978cd3b83af92750279eec48af457b96b
Christian Rolle <1366842+monxoom@users.noreply.github.com>
2025-03-18 16:02:18 +0100

    fix code for Symfony 5.4

--- src/Security/ContaoBackendLogin.php
+++ src/Security/ContaoBackendLogin.php
@@ -1,7 +1,6 @@
 <?php
 namespace Comolo\SuperLoginClient\ContaoEdition\Security;
 
-
 use Comolo\SuperLoginClient\ContaoEdition\User\RemoteUserInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,18 +10,16 @@ use Contao\CoreBundle\Security\User\ContaoUserProvider;
 use Contao\CoreBundle\Framework\ContaoFrameworkInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class ContaoBackendLogin
 {
     protected const SECURED_AREA = 'contao_backend';
-	protected $contaoUserProvider;
-	protected $session;
-	protected $tokenStorage;
-	protected $authenticationManager;
-	protected $eventDispatcher;
-	protected $requestStack;
+    protected $contaoUserProvider;
+    protected $session;
+    protected $tokenStorage;
+    protected $eventDispatcher;
+    protected $requestStack;
 
     /**
      * ContaoBackendLogin constructor.
@@ -30,38 +27,36 @@ class ContaoBackendLogin
      * @param SessionInterface $session
      * @param ContaoFrameworkInterface $contaoFramework
      * @param TokenStorageInterface $tokenStorage
-     * @param AuthenticationManagerInterface $authenticationManager
      * @param EventDispatcherInterface $eventDispatcher
      * @param RequestStack $requestStack
      */
-	public function __construct(
-	    ContaoUserProvider $contaoUserProvider,
+    public function __construct(
+        ContaoUserProvider $contaoUserProvider,
         SessionInterface $session,
         ContaoFrameworkInterface $contaoFramework,
         TokenStorageInterface $tokenStorage,
-        AuthenticationManagerInterface $authenticationManager,
         EventDispatcherInterface $eventDispatcher,
         RequestStack $requestStack
-
     ) {
         $this->contaoUserProvider = $contaoUserProvider;
         $this->session = $session;
         $this->tokenStorage = $tokenStorage;
-        $this->authenticationManager = $authenticationManager;
         $this->eventDispatcher = $eventDispatcher;
         $this->requestStack = $requestStack;
 
-        if (!$contaoFramework->isInitialized()) $contaoFramework->initialize();
+        if (!$contaoFramework->isInitialized()) {
+            $contaoFramework->initialize();
+        }
     }
 
     /**
      * @param RemoteUserInterface $remoteUser
      */
-    public function login(RemoteUserInterface $remoteUser)
+    public function login(RemoteUserInterface $remoteUser): void
     {
-		$user = $this->contaoUserProvider->loadUserByUsername($remoteUser->getUsername());
+        $user = $this->contaoUserProvider->loadUserByUsername($remoteUser->getUsername());
 
-		$token = new UsernamePasswordToken($user, null, self::SECURED_AREA, $user->getRoles());
+        $token = new UsernamePasswordToken($user, null, self::SECURED_AREA, $user->getRoles());
         $this->tokenStorage->setToken($token);
 
         $this->session->set('_security_' . self::SECURED_AREA, serialize($token));
